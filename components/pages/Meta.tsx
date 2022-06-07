172e3d2bbd824d3423cc412322bb775ff9af3dea
subaru-git <subaru1208.m@gmail.com>
2022-06-07 14:32:09 +0900

    update meta

--- components/pages/Meta.tsx
+++ components/pages/Meta.tsx
@@ -5,9 +5,41 @@ const Meta: FC = () => {
   return (
     <Head>
       <title>Mob Timer</title>
+      <meta charSet="utf-8" />
+      <link
+        rel="icon"
+        href="%PUBLIC_URL%/mobtimer-logo.svg"
+        type="image/svg+xml"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content="The timer for mob programming" />
-      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        href="%PUBLIC_URL%/mobtimer-logo.svg"
+        type="image/svg+xml"
+      />
+      <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+      <meta property="og:url" content="https://mobtimer.space/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Mob Timer" />
+      <meta
+        property="og:description"
+        content="Timer for mobbing. This Web App notifies driver changing and break time."
+      />
+      <meta property="og:site_name" content="Mob Timer" />
+      <meta
+        property="og:image"
+        content="https://github.com/subaru-git/mob-timer/blob/master/public/card.png?raw=true"
+      />
+      <meta name="twitter:card" content="summary" />
       <meta name="robots" content="noindex" />
+      <script
+        data-ad-client="ca-pub-4685158362902526"
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      ></script>
+      {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+      <script src="https://widget.plog.cash/plog.js"></script>
     </Head>
   );
 };
