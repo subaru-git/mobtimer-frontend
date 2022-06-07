a98c194bb3497b54743b535ab6196d911556b230
subaru-git <subaru1208.m@gmail.com>
2022-06-07 14:56:53 +0900

    update meta

--- components/pages/Meta.tsx
+++ components/pages/Meta.tsx
@@ -6,26 +6,19 @@ const Meta: FC = () => {
     <Head>
       <title>Mob Timer</title>
       <meta charSet="utf-8" />
-      <link
-        rel="icon"
-        href="%PUBLIC_URL%/mobtimer-logo.svg"
-        type="image/svg+xml"
-      />
+      <link rel="icon" href="/mobtimer-logo.svg" type="image/svg+xml" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content="The timer for mob programming" />
       <link
         rel="apple-touch-icon"
-        href="%PUBLIC_URL%/mobtimer-logo.svg"
+        href="/mobtimer-logo.svg"
         type="image/svg+xml"
       />
-      <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+      <link rel="manifest" href="/manifest.json" />
       <meta property="og:url" content="https://mobtimer.space/" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Mob Timer" />
-      <meta
-        property="og:description"
-        content="Timer for mobbing. This Web App notifies driver changing and break time."
-      />
+      <meta property="og:description" content="The timer for mob programming" />
       <meta property="og:site_name" content="Mob Timer" />
       <meta
         property="og:image"
