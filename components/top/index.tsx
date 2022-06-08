8861cad6d1683560b1da90da6bd97763806bbaa5
subaru-git <subaru1208.m@gmail.com>
2022-06-08 15:14:00 +0900

    reafactor top

--- components/top/index.tsx
+++ components/top/index.tsx
@@ -1,33 +1,37 @@
 import React, { FC, useState } from "react";
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
 import {
   Input,
   InputGroup,
   InputLeftAddon,
   Button,
-  Flex,
   Image,
   VStack,
   HStack,
-  Box,
   Center,
+  Flex,
 } from "@chakra-ui/react";
-import { useRouter } from "next/router";
-import dynamic from "next/dynamic";
-import Footer from "../pages/Footer";
+import { MdOutlineArrowForwardIos } from "react-icons/md";
 let AppBar = dynamic(() => import("../pages/AppBar"), { ssr: false });
+import Footer from "../pages/Footer";
 
 const Top: FC = () => {
   const [name, setName] = useState("");
   const router = useRouter();
   return (
-    <>
+    <Flex direction="column" h="100vh" justifyContent="space-between">
       <AppBar top={true} />
-      <Center w="100%" h="calc(100vh - 56px - 24px)">
+      <Center>
         <VStack>
-          <Image src="/mobtimer-logo.svg" alt="Logo" boxSize="250px" />
+          <Image src="/mobtimer-logo.svg" alt="Logo" boxSize="250px" m={16} />
           <HStack>
             <InputGroup>
-              <InputLeftAddon children="https://mobtimer.space/" />
+              <InputLeftAddon
+                children={window.location.href}
+                bg="teal.300"
+                color="white"
+              />
               <Input
                 placeholder="mob-room"
                 onChange={(e) => {
@@ -36,18 +40,21 @@ const Top: FC = () => {
               />
             </InputGroup>
             <Button
+              color="teal.300"
+              variant="outline"
+              borderColor="teal.300"
               onClick={() => {
                 router.push(`/${name}`);
               }}
               disabled={!name}
             >
-              {">"}
+              <MdOutlineArrowForwardIos color="teal" />
             </Button>
           </HStack>
         </VStack>
       </Center>
       <Footer />
-    </>
+    </Flex>
   );
 };
 
