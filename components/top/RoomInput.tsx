6c17731277705ddad45c57b401559c273f43661a
subaru-git <subaru1208.m@gmail.com>
2022-06-08 17:13:17 +0900

    fix error

---
+++ components/top/RoomInput.tsx
@@ -0,0 +1,45 @@
+import React, { FC, useState } from "react";
+import { useRouter } from "next/router";
+import {
+  Button,
+  HStack,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+} from "@chakra-ui/react";
+import { MdOutlineArrowForwardIos } from "react-icons/md";
+
+const RoomInput: FC = () => {
+  const [name, setName] = useState("");
+  const router = useRouter();
+  return (
+    <HStack>
+      <InputGroup>
+        <InputLeftAddon
+          children={window.location.href}
+          bg="teal.300"
+          color="white"
+        />
+        <Input
+          placeholder="mob-room"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+      </InputGroup>
+      <Button
+        color="teal.300"
+        variant="outline"
+        borderColor="teal.300"
+        onClick={() => {
+          router.push(`/${name}`);
+        }}
+        disabled={!name}
+      >
+        <MdOutlineArrowForwardIos color="teal" />
+      </Button>
+    </HStack>
+  );
+};
+
+export default RoomInput;
--- components/top/index.tsx
+++ components/top/index.tsx
@@ -1,56 +1,19 @@
 import React, { FC, useState } from "react";
 import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
-import {
-  Input,
-  InputGroup,
-  InputLeftAddon,
-  Button,
-  Image,
-  VStack,
-  HStack,
-  Center,
-  Flex,
-} from "@chakra-ui/react";
-import { MdOutlineArrowForwardIos } from "react-icons/md";
+import { Image, VStack, Center, Flex } from "@chakra-ui/react";
 let AppBar = dynamic(() => import("../pages/AppBar"), { ssr: false });
+let RoomInput = dynamic(() => import("./RoomInput"), { ssr: false });
 import Footer from "../pages/Footer";
 
 const Top: FC = () => {
-  const [name, setName] = useState("");
-  const router = useRouter();
   return (
     <Flex direction="column" h="100vh" justifyContent="space-between">
       <AppBar top={true} />
       <Center>
         <VStack>
           <Image src="/mobtimer-logo.svg" alt="Logo" boxSize="250px" m={16} />
-          <HStack>
-            <InputGroup>
-              <InputLeftAddon
-                children={window.location.href}
-                bg="teal.300"
-                color="white"
-              />
-              <Input
-                placeholder="mob-room"
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-              />
-            </InputGroup>
-            <Button
-              color="teal.300"
-              variant="outline"
-              borderColor="teal.300"
-              onClick={() => {
-                router.push(`/${name}`);
-              }}
-              disabled={!name}
-            >
-              <MdOutlineArrowForwardIos color="teal" />
-            </Button>
-          </HStack>
+          <RoomInput />
         </VStack>
       </Center>
       <Footer />
